d4c6289e646d297d3653dc1163d50372adc5929b
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-04-03 14:26:21 +0300

    fixed some problems

--- Lab1/trapezoidal_decomposition.py
+++ Lab1/trapezoidal_decomposition.py
@@ -50,6 +50,12 @@ class Leaf:
         self.left = left
         self.right = right
         self.split_edge = split_edge
+        
+    def locate(self, p: Point):
+        if is_not_right(p, self.split_edge):
+            self.left.locate(p)
+        else:
+            self.right.locate(p)
     
     def display(self):
         print("LEFT -> ")
@@ -58,9 +64,16 @@ class Leaf:
         self.right.display()
     
 class Root:
-    def __init__(self, lower: Leaf, upper: Leaf) -> None:
+    def __init__(self, lower: Leaf, upper: Leaf, med: int) -> None:
         self.lower = lower
         self.upper = upper
+        self.med = med
+        
+    def locate(self, p: Point):
+        if p.y >= self.med:
+            self.upper.locate(p)
+        else:
+            self.lower.locate(p)
         
     def display(self):
         print("LOWER -> ")
@@ -71,6 +84,9 @@ class Root:
 class Tree:
     def __init__(self, root: Root) -> None:
         self.root = root
+        
+    def locate(self, p: Point):
+        self.root.locate(p)
     
     def display(self):
         print("ROOT -> ")
@@ -80,6 +96,13 @@ class Tree:
 class Trapezoid:
     def __init__(self, edges: List[Edge]) -> None:
         self.edges = edges
+        
+    def locate(self, p: Point):
+        print("POINT:")
+        p.print()
+        print("FOUND IN TRAPEZOID:")
+        for e in self.edges:
+            e.print()
     
     def display(self):
         print("\nTRAPEZOID")
@@ -115,6 +138,14 @@ def get_left_edge(vertices: List[Point]):
             left_bound = v
     return Edge(Point(left_bound.x, MIN_Y), Point(left_bound.x, MAX_Y))
 
+def is_not_right(p: Point, e: Edge):
+    vec1 = Point(e.end.x - e.start.x, e.end.y - e.start.y)
+    vec2 = Point(p.x - e.start.x, p.y - e.start.y)
+    cross = vec1.x * vec2.y - vec1.y * vec2.x
+    if cross > 0:
+        return True
+    else:
+        return False
 
 def decompose_root(edges: List[Edge], vertices: List[Point], lower_edge: Edge, upper_edge: Edge, right_edge: Edge, left_edge: Edge):
     med = get_med(vertices)
@@ -132,7 +163,7 @@ def decompose_root(edges: List[Edge], vertices: List[Point], lower_edge: Edge, u
             lower_v.append(v)
         else:
             upper_v.append(v)
-    return Root(decompose_leaves(lower, lower_v, lower_edge, med, right_edge, left_edge), decompose_leaves(upper, upper_v, med, upper_edge, right_edge, left_edge))
+    return Root(decompose_leaves(lower, lower_v, lower_edge, med, right_edge, left_edge), decompose_leaves(upper, upper_v, med, upper_edge, right_edge, left_edge), med.start.y)
 
 def decompose_leaves(edges: List[Edge], vertices: List[Point], lower_edge: Edge, upper_edge: Edge, right_edge: Edge, left_edge: Edge):
     split_edge = None
@@ -141,7 +172,7 @@ def decompose_leaves(edges: List[Edge], vertices: List[Point], lower_edge: Edge,
         if e.start.y <= lower_edge.start.y and e.end.y >= upper_edge.start.y:
             split_edge = e
             break
-        if ((e.start.y > lower_edge.start.y and e.start.y < upper_edge.start.y) or (e.end.y > lower_edge.start.y and e.end.y < upper_edge.start.y)):
+        if (is_not_right(e.start, right_edge) and not is_not_right(e.start, left_edge) and e.start.y != upper_edge.start.y and e.start.y != lower_edge.start.y) or (is_not_right(e.end, right_edge) and not is_not_right(e.end, left_edge) and e.end.y != upper_edge.start.y and e.end.y != lower_edge.start.y):
             inner_point = True
     if split_edge == None:
         if(inner_point == False):
@@ -157,9 +188,9 @@ def decompose_leaves(edges: List[Edge], vertices: List[Point], lower_edge: Edge,
             continue
         if e.start.x < split_edge.start.x or e.start.x < split_edge.end.x or e.end.x < split_edge.start.x or e.end.x < split_edge.end.x:
             left_edges.append(e)
-            if(e.start.y >= lower_edge.start.y and e.start.y <= upper_edge.start.y and not right_verts.__contains__(e.start)):
+            if(e.start.y >= lower_edge.start.y and e.start.y <= upper_edge.start.y and not left_verts.__contains__(e.start)):
                 left_verts.append(e.start)
-            if(e.end.y >= lower_edge.start.y and e.end.y <= upper_edge.start.y and not right_verts.__contains__(e.end)):
+            if(e.end.y >= lower_edge.start.y and e.end.y <= upper_edge.start.y and not left_verts.__contains__(e.end)):
                 left_verts.append(e.end)
         if e.start.x > split_edge.start.x or e.start.x > split_edge.end.x or e.end.x > split_edge.start.x or e.end.x > split_edge.end.x:
             right_edges.append(e)
@@ -169,12 +200,14 @@ def decompose_leaves(edges: List[Edge], vertices: List[Point], lower_edge: Edge,
                 right_verts.append(e.end)
     return Leaf(right = decompose_leaves(edges=right_edges, vertices=right_verts, lower_edge=lower_edge, upper_edge=upper_edge, left_edge=split_edge, right_edge=right_edge), left = decompose_leaves(edges=left_edges, vertices=left_verts, lower_edge=lower_edge, upper_edge=upper_edge, left_edge=left_edge, right_edge=split_edge), split_edge=split_edge)
     
-def plot_plane(vertices, points):
-    x = [v.x for v in vertices]
-    y = [v.y for v in vertices]
-    plt.plot(x + [x[0]], y + [y[0]], 'b-') 
+def plot_plane(edges, points):
+    # x = [v.x for v in vertices]
+    # y = [v.y for v in vertices]
+    # plt.plot(x + [x[0]], y + [y[0]], 'b-') 
+    for e in edges:
+        plt.plot([e.start.x, e.end.x], [e.start.y, e.end.y], 'b-')
     for point in points:
-        plt.scatter(point[0], point[1], color="red", label="Point")
+        plt.scatter(point.x, point.y, color="red", label="Point")
     plt.gca().set_aspect('equal', adjustable='box')
     plt.xlabel('X')
     plt.ylabel('Y')
@@ -182,7 +215,7 @@ def plot_plane(vertices, points):
     plt.grid(True)
     plt.xlim(MIN_X-1, MAX_X)
     plt.ylim(MIN_Y-1, MAX_Y)
-    #plt.show()
+    plt.show()
 
 
 def plot_trapezoid(edges: List[Edge]):
@@ -205,18 +238,24 @@ def vert_sort(arr):
             j -= 1
         arr[j + 1] = key
     
-        
 
-# vertices = [Point(0, 1.5), Point(1, 3.5), Point(3.5, 4), Point(5, 2), Point(2.5, 0)]
-# edges = [Edge(vertices[0], vertices[1]), Edge(vertices[1], vertices[2]), Edge(vertices[2], vertices[3]), Edge(vertices[3], vertices[4]), Edge(vertices[4], vertices[0])]
-vertices = [Point(0, 1.5), Point(1, 3.5), Point(3.5, 4), Point(5, 2), Point(2.5, 0)]
-edges = [Edge(vertices[0], vertices[1]), Edge(vertices[1], vertices[2]), Edge(vertices[2], vertices[3]), Edge(vertices[3], vertices[4]), Edge(vertices[3], vertices[1]), Edge(vertices[4], vertices[0])]
+def run():
+   # vertices = [Point(0, 1.5), Point(1, 3.5), Point(3.5, 4), Point(5, 2), Point(2.5, 0)]
+    # edges = [Edge(vertices[0], vertices[1]), Edge(vertices[1], vertices[2]), Edge(vertices[2], vertices[3]), Edge(vertices[3], vertices[4]), Edge(vertices[4], vertices[0])]
+    # vertices = [Point(0, 1.5), Point(1, 3.5), Point(3.5, 4), Point(5, 2), Point(2.5, 0)]
+    # edges = [Edge(vertices[0], vertices[1]), Edge(vertices[1], vertices[2]), Edge(vertices[2], vertices[3]), Edge(vertices[3], vertices[4]), Edge(vertices[3], vertices[1]), Edge(vertices[4], vertices[0])]
+    vertices = [Point(0, 0), Point(0.2, 3), Point(0.5, 2.5), Point(0.7, 2.8), Point(1, 2.3), Point(1.2, 2.7), Point(1.3, 0)]
+    edges = [Edge(vertices[0], vertices[1]), Edge(vertices[1], vertices[2]), Edge(vertices[2], vertices[3]), Edge(vertices[3], vertices[4]), Edge(vertices[4], vertices[5]), Edge(vertices[5], vertices[6]), Edge(vertices[6], vertices[0])]
+    points = []
+    for i in range(5):
+        point = Point(random.uniform(0, 6), random.uniform(0,3))  # Point to test
+        points.append(point)
+    plot_plane(edges, points)
+    vert_sort(vertices)
+    tree = Tree(decompose_root(edges, vertices, get_lower_edge(vertices), get_upper_edge(vertices), get_right_edge(vertices), get_left_edge(vertices)))
+    for p in points:
+        tree.locate(p)
+    tree.display()     
 
-points = []
-for i in range(5):
-    point = np.array([random.uniform(0, 6), random.uniform(0,3)])  # Point to test
-    points.append(point)
-plot_plane(vertices, points)
-vert_sort(vertices)
-tree = Tree(decompose_root(edges, vertices, get_lower_edge(vertices), get_upper_edge(vertices), get_right_edge(vertices), get_left_edge(vertices)))
-tree.display()
+run()
+    
